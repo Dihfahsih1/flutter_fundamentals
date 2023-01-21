631f0113b834f9ec19af0193298124524e782142
dev <dihfahsihm@gmail.com>
2023-01-22 00:14:40 +0300

    create the first app form field to allow input

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -14,6 +14,32 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
       appBar: AppBar(
         title: Text('Select Range'),
       ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Minimum',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_forward),
+          onPressed: () {
+            // TODO: Validate the form
+            // TODO: Navigate to the next page
+          }),
     );
   }
 }
